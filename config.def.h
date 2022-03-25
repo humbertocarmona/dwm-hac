67ce23a5019f368b693a2dc667447e845209595f
humberto <carmona@fisica.ufc.br>
2022-03-25 12:17:44 -0300

    floating windows e rofi em C-o

--- config.def.h
+++ config.def.h
@@ -4,13 +4,13 @@
 #define TERMINAL "kitty"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
+static unsigned int snap      = 12;       /* snap pixel */
 static unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
@@ -47,17 +47,20 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
     */
-    /* class    instance      title          tags mask    isfloating   isterminal  noswallow  monitor */
-    { "Gimp",     NULL,       NULL,             1 << 8,       0,           0,         0,        -1 },
-    { "Inkscape", NULL,       NULL,             1 << 7,       0,           0,         0,        -1 },
-    { "Qgis",     NULL,       NULL,             1 << 6,       0,           0,         0,        -1 },
-    { "Alacritty",NULL,       NULL,             0,            0,           1,         0,        -1 },
-    { "Kitty",    NULL,       NULL,             0,            0,           1,         0,        -1 },
-    { "St",       NULL,       NULL,             0,            0,           1,         0,        -1 },
-    { NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
-    { NULL,      "bg",        NULL,             0,            0,           1,    1 << 7,        -1 },
-    { NULL,      "spterm",    NULL,             SPTAG(0),     1,           1,         0,        -1 },
-    { NULL,      "spcalc",    NULL,             SPTAG(1),     1,           1,         0,        -1 },
+    /* class    instance            title          tags mask    isfloating   isterminal  noswallow  monitor */
+    { "Gimp",           NULL,       NULL,             1 << 8,       0,           0,         0,        -1 },
+    { "Inkscape",       NULL,       NULL,             1 << 7,       0,           0,         0,        -1 },
+    { "Qgis",           NULL,       NULL,             1 << 6,       0,           0,         0,        -1 },
+    { "Alacritty",      NULL,       NULL,             0,            0,           1,         0,        -1 },
+    { "Kitty",          NULL,       NULL,             0,            0,           1,         0,        -1 },
+    { "St",             NULL,       NULL,             0,            0,           1,         0,        -1 },
+    { NULL,             "spterm",   NULL,             SPTAG(0),     1,           1,         0,        -1 },
+    { NULL,             "spcalc",   NULL,             SPTAG(1),     1,           1,         0,        -1 },
+    { "Pavucontrol",    NULL,       NULL,             0,            1,           0,         0,        -1 },
+    { "Bluman-manager", NULL,       NULL,             0,            1,           0,         0,        -1 },
+    { "matplotlib",     NULL,       NULL,             0,            1,           0,         0,        -1 },
+    { "vlc",            NULL,       NULL,             0,            1,           0,         0,        -1 },
+    { NULL,             NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 };
 
 /* layout(s) */
@@ -194,8 +197,6 @@ static Key keys[] = {
     TAGKEYS(                XK_9,               8)
     { MODKEY,               XK_0,               view,       {.ui = ~0 } },
     { MODKEY|ShiftMask,     XK_0,               tag,        {.ui = ~0 } },
-    /* { MODKEY|ShiftMask,  XK_Escape,          spawn,      SHCMD("") }, */
-    /* { MODKEY|ShiftMask,  XK_grave,           togglescratch,  SHCMD("") }, */
     { MODKEY,               XK_minus,           spawn,      SHCMD("pamixer --allow-boost -d 5; kill -44 $(pidof dwmblocks)") },
     { MODKEY|ShiftMask,     XK_minus,           spawn,      SHCMD("pamixer --allow-boost -d 15; kill -44 $(pidof dwmblocks)") },
     { MODKEY,               XK_equal,           spawn,      SHCMD("pamixer --allow-boost -i 5; kill -44 $(pidof dwmblocks)") },
@@ -204,8 +205,7 @@ static Key keys[] = {
     /* { MODKEY|ShiftMask,  XK_Tab,             spawn,      SHCMD("") }, */
     { MODKEY,               XK_e,               spawn,      SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook") },
     { MODKEY|ShiftMask,     XK_e,               spawn,      SHCMD(TERMINAL " -e abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook") },
-    { MODKEY,               XK_r,               spawn,      SHCMD("rofi -show combi -combi-modi 'window,drun,ssh' -modi combi -show-icons") },
-    { MODKEY|ShiftMask,     XK_r,               spawn,      SHCMD(TERMINAL " -e lf") },
+    { MODKEY,               XK_f,               togglefullscr,  {0} },
     { MODKEY,               XK_t,               setlayout,  {.v = &layouts[0]} }, /* tile */
     { MODKEY|ShiftMask,     XK_t,               setlayout,  {.v = &layouts[1]} }, /* bstack */
     { MODKEY,               XK_y,               setlayout,  {.v = &layouts[2]} }, /* spiral */
@@ -214,16 +214,19 @@ static Key keys[] = {
     { MODKEY|ShiftMask,     XK_u,               setlayout,  {.v = &layouts[5]} }, /* monocle */
     { MODKEY,               XK_i,               setlayout,  {.v = &layouts[6]} }, /* centeredmaster */
     { MODKEY|ShiftMask,     XK_i,               setlayout,  {.v = &layouts[7]} }, /* centeredfloatingmaster */
-    { MODKEY,               XK_o,               incnmaster,     {.i = +1 } },
-    { MODKEY|ShiftMask,     XK_o,               incnmaster,     {.i = -1 } },
-    { MODKEY,               XK_backslash,       view,       {0} },
     { MODKEY,               XK_a,               togglegaps, {0} },
+    { MODKEY,               XK_backslash,       view,       {0} },
     { MODKEY|ShiftMask,     XK_a,               defaultgaps,{0} },
     { MODKEY,               XK_b,               spawn,      SHCMD("$BROWSER") },
-    { MODKEY,               XK_s,               togglesticky,{0} },
+    { MODKEY,               XK_c,               spawn,      {.v = spcmd1} },
+    { MODKEY|ShiftMask,     XK_c,               spawn,      {.v = spcmd2} },
+    /* { MODKEY|ShiftMask,  XK_o,               incnmaster,     {.i = -1 } }, */
+    /* { MODKEY,            XK_o,               incnmaster,     {.i = +1 } }, */
+    { MODKEY,               XK_o,               spawn,      SHCMD("rofi -show combi -combi-modi 'window,drun,ssh' -modi combi -show-icons") },
     { MODKEY,               XK_p,               spawn,      SHCMD("dmenu_run -fn 'Hack NF:size=10'") },
-    { MODKEY|ShiftMask,     XK_p,               spawn,      SHCMD("passmenu") },
-    { MODKEY,               XK_f,               togglefullscr,  {0} },
+    { MODKEY|ShiftMask,     XK_p,               spawn,      SHCMD("passmenu -fn 'Hack NF:size=10'") },
+    { MODKEY|ShiftMask,     XK_r,               spawn,      SHCMD(TERMINAL " -e lf") },
+    { MODKEY,               XK_s,               togglesticky,{0} },
     { MODKEY|ShiftMask,     XK_f,               setlayout,  {.v = &layouts[8]} },
     { MODKEY,               XK_g,               shiftview,  { .i = -1 } },
     { MODKEY|ShiftMask,     XK_g,               shifttag,   { .i = -1 } },
@@ -237,7 +240,7 @@ static Key keys[] = {
     { MODKEY,               XK_Return,          spawn,      {.v = termcmd } },
     { MODKEY|ShiftMask,     XK_Return,          togglescratch,  {.ui = 0} },
     { MODKEY,               XK_w,               killclient, {0} },
-    { MODKEY,               XK_q,               quit,       {0} },
+    { MODKEY|ShiftMask,     XK_q,               spawn,      SHCMD("sysact") },
     /*{ MODKEY|ShiftMask,   XK_x,               spawn,      SHCMD(TERMINAL " -e sudo nmtui") },*/
     { MODKEY,               XK_x,               incrgaps,   {.i = -3 } },
     { MODKEY,               XK_z,               incrgaps,   {.i = +3 } },
@@ -268,7 +271,7 @@ static Key keys[] = {
     { MODKEY,               XK_F11,             spawn,      SHCMD("mpv --untimed --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
     { MODKEY,               XK_F12,             spawn,      SHCMD("remaps & notify-send \\\"⌨️ Keyboard remapping...\\\" \\\"Re-running keyboard defaults for any newly plugged-in keyboards.\\\"") },
     { MODKEY,               XK_space,           zoom,       {0} },
-    { MODKEY,               XK_b,               togglebar,  {0} },
+    { MODKEY|ShiftMask,     XK_b,               togglebar,  {0} },
     { MODKEY|ShiftMask,     XK_space,           togglefloating, {0} },
 
     /* { 0,                    XK_Print,           spawn,      SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") }, */
